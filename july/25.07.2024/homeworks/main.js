daa54f03d9f4570ebd19e8bf1a869809c6bfd045
lirazbarney <107267082+lirazbarney@users.noreply.github.com>
2024-07-25 16:24:57 +0300

    doing hw

---
+++ july/25.07.2024/homeworks/main.js
@@ -0,0 +1,305 @@
+{ //Arithmetic Operations Quiz 
+// //task 1
+// console.log(`task 1: the sum of 3 and 5 is ${3+5}`);
+
+// //task 2
+// console.log("task 2: to add 2 numbers in js you wil use \"+\"");
+
+// //task 3
+// console.log("task 3: the symbol that is used in js for subtraction is \"-\"");
+
+// //task 4
+// console.log(`task 4: the result of 10-4 in js is ${10-4}`);
+
+// //task 5
+// console.log("task 5: to multiply 2 numbers in js you wil use \"*\"");
+
+// //task 6
+// console.log(`task 6: the result of 6*2 in js is ${6*2}`);
+
+// //task 7
+// console.log("task 7: the symbol that is used in js for division is \"/\"");
+
+// //task 8
+// console.log(`task 8: the result of 15/3 in js is ${15/3}`);
+
+// //task 9
+// console.log(`task 9: this is what will happen if ypu will try to divide a number by 0 in js: ${10/0}`);
+
+// //task 10
+// console.log("task 10: to calculate the reminder of a division in js you wil use \"%\"");
+
+// //task 11
+// console.log(`task 11: the result of 17%5 in js is ${17%5}`);
+
+// //task 12
+// console.log("task 12: to add 1 to a variable in js you have three options:");
+// console.log("1. varName = varName + 1;");
+// console.log("2. varName += 1;");
+// console.log("3. varName ++;");
+
+// //task 13
+// console.log("task 13: there is no difference between x += 1 and x = x + 1. they both achieve the same purpose");
+
+// //task 14
+// console.log("task 14: to subtract 1 from a variable in js you have to do the same things as in adding one but with \"-\" and not \"+\"");
+
+// //task 15
+// console.log(`task 15: the result of 10 + 5 * 2 in js is ${10 + 5 * 2} because 5*2 is ${5*2} and + 10 is ${(5*2)+10}`);
+
+// //task 16
+// console.log("task 16: to change the order of arithmetics operations in js you will need to use \"()\" around what you want to be first");
+
+// //task 17
+// console.log(`task 17: the result of (10 + 5) * 2 in js is ${(10 + 5) * 2}`);
+
+// //task 18
+// console.log("task 18: to change \"5\" into 5 you can do \'Number(\"5\")\' or +\"5\"");
+
+// //task 19
+// console.log(`task 19: the result of \"5\" + 3 in js is supposed to be a warning like any normal language but bc its js is ${"5" + 3}`);
+
+// //task 20
+// console.log(`task 20: the result of \"10\" - 5 in js, like the + sign, it supposed to be an error like any other languages but in js is ${"10" - 5}`);
+}
+
+{ //Assignment Operators Quiz
+// //task 1
+// console.log("task 1: to the basic assigning operator is \"=\"");
+
+// //task 2
+// console.log("task 2: to assign a value to a variable in js you will use the variable name, the \"=\" and the value like this: varName = \"some value\"");
+
+// //task 3
+// console.log("task 3: the += operator increase the variable with the value written next to it");
+
+// //task 4
+// console.log(`task 4: to add 5 to a variable x using the += operator you will have to do x += 5`);
+
+// //task 5
+// console.log(`task 5:  the -= operator decrease the variable with the value written next to it`);
+
+// //task 6
+// console.log(`task 6: to subtract 3 from a variable y using the -= operator you will have to do y -= 3`);
+
+// //task 7
+// console.log(`task 7:  the *= operator multiply the variable with the value written next to it`);
+
+// //task 8
+// console.log(`task 8: to multiply 2 from a variable z using the *= operator you will have to do z *= 2`);
+
+// //task 9
+// console.log(`task 9:  the /= operator divide the variable with the value written next to it`);
+
+// //task 10
+// console.log(`task 10: to divide 4 from a variable w using the /= operator you will have to do w *= 4`);
+
+// //task 11
+// console.log("task 11: there is no difference between x = x + 1 and x += 1. they both achieve the same purpose");
+
+// //task 12
+// console.log("task 12: to increase a variable by 1 using an assignment operator in js you have three options:");
+// console.log("1. varName = varName + 1;");
+// console.log("2. varName += 1;");
+// console.log("3. varName ++;");
+
+// //task 13
+// let greeting = "hello, ";
+// greeting += "world!";
+// console.log(`task 14: the result of using += with 2 string is this: ${greeting}`);
+
+// //task 14
+// console.log("task 14: to add \"hello\" at the end of a variable str you will need to do str += \"hello\"");
+
+// //task 15
+// let x = 5; 
+// x += 3;
+// console.log(`task 15: the result of \"let x = 5; x += 3;\" in js is ${x}`);
+}
+
+{//Comparison Operators Quiz
+// //task 1
+// console.log(`task 1: the equality operator in js is somehow \"===\" while in other normal languages is just \"==\"`);
+
+// //task 2
+// console.log(`task 2: the difference between \"==\" and "===" is that "==" check the equality without checking its type and "===" check both the content and the type`);
+
+// //task 3
+// console.log(`task 3: to check if 2 values are equal you will use === between them in "if" statement`);
+
+// //task 4
+// console.log(`task 4: the > operator checks if the left value is grater than the right value`);
+
+// //task 5
+// console.log(`task 5: the < operator checks if the left value is less than the right value`);
+
+// //task 6
+// console.log(`task 6: to check if some value is grater or equal to another value you will use >=`);
+
+// //task 7
+// console.log(`task 7: to check if some value is less or equal to another value you will use <=`);
+
+// //task 8
+// console.log(`task 8: the result of 5 > 3 is ${5 > 3}`);
+
+// //task 9
+// console.log(`task 9: js is a weird language and instead of yelling an error, in js 10 === "10" will be ${10 === "10"}`);
+
+// //task 10
+// console.log(`task 10: the result of 7 <= 7 in js is ${7 <= 7}`);
+
+// //task 11
+// console.log(`task 11: to compare 2 strings alphabetically you will use regular comparing operator like > or <. the first they will appear in dictionaries the lower value they will get`);
+
+// //task 12
+// console.log(`task 12: the result of "apple" < "banana" is ${"apple" < "banana"}`);
+
+// //task 13
+// console.log(`task 13: the !== operator checks if the 2 values are different (if (val1 === val2) === false`);
+
+// //task 14
+// console.log(`task 14: the result of 5 !== "5" is ${5 !== "5"}`);
+
+// //task 15
+// console.log(`task 15: using the === comparison dirent data type would consider different`);
+
+}
+
+{//Data Types Quiz
+// //task 1
+// console.log(`task 1: the data type of x when "x = 5" is number (why not int like any other languages ):`);
+
+// //task 2
+// console.log(`task 2: to declare a string variable in js you have to do so: let str = "some string"`);
+
+// // task 3
+// console.log(`task 3: the differences between null and undefined is that one is default state and the other is explicitly set`);
+
+// // task 4
+// console.log(`task 4: to check a variable type you will have to use the "typeof" function and will pass in the () the variable`);
+
+// // task 5
+// console.log(`task 5: the data type of 5 / 2 is a ${typeof(5 / 2)}`);
+
+// // task 6
+// console.log(`task 6: the data type of true is called boolean`);
+
+// // task 7
+// console.log(`task 7: to declare an object in js you will have to do this: let name {var1: val, var2: val}`);
+
+// // task 8
+// console.log(`task 8: the data type of NaN is actually number (typeof(NaN) = ${typeof(NaN)})`);
+
+// // task 9
+// console.log(`task 9: the difference between \"==\" and "===" is that "==" check the equality without checking its type and "===" check both the content and the type`);
+
+// // task 10
+// console.log(`task 10: to convert a sting to a number you will use the Number function and pass the string in the ()`);
+
+// // task 11
+// console.log(`task 11: the result of "5" + 3 is ${typeof("5" + 3)}`);
+
+// // task 12
+// console.log(`task 12: to check if a variable is from a specific type you will have to compare the typeof(variable) to the specific type you want`);
+
+// // task 13
+// console.log(`task 13: to create multi lines string you will have to use the \' \' marks`);
+
+// // task 14
+// console.log(`task 14: while ' and " are the same, \` allow for more additional features like \$\{\} and multi line strings`);
+
+// // task 15
+// console.log(`task 15: to convert a number to a string you will have to use the String function and pass the number to it)}`);
+
+// // task 16
+// console.log(`task 16: the result of typeof(null) is ${typeof(null)}`);
+
+// // task 17
+// console.log(`task 17: to declare a constant in js you will use the word "const" instead of let`);
+
+// // task 18
+// console.log(`task 18: when adding a string and a number in js (unlike any other normal languages), the result will be string`)
+
+// // task 19
+// console.log(`task 19: the result of 3 > 2 is ${typeof(3 > 2)}`);
+
+// // task 20
+// console.log(`task 20: to escape spacial characters in string you will need to use the backslash before it - \\`);
+
+// // task 21
+// console.log(`task 21: while const will not allow you to change its value, let and var will allow you to. the difrence between them is mostly about scoping`);
+
+// // task 22
+// console.log(`task 22: to check if a variable is undefined you will need to compare the variable to undefined`);
+
+// // task 23
+// console.log(`task 23: the result of 10 + "5" is ${10 + "5"}`);
+
+// // task 24
+// console.log(`task 24: to create a boolean variable you will need to first assign it with either true or false values`);
+
+// // task 25
+// console.log(`task 25: the primitive data types is regular data types while objects are built from different primitive data type and are transferred as a reference`);
+}
+
+{//Logical Operators Quiz
+//task 1
+console.log(`task 1: the 3 main logical operators in js are - and (&&), or (||) and not (!)`);
+
+//task 2
+console.log(`task 2: the and (&&) operators return true if all the values are either true or truthy`);
+
+//task 3
+console.log(`task 3: the result of true && true is ${true && true}`);
+
+//task 4
+console.log(`task 4: the result of true && false is ${true && false}`);
+
+//task 5
+console.log(`task 5: the or (||) operator return true if at least one of the values is either true or truthy`);
+
+//task 6
+console.log(`task 6: the result of false || true is ${false || true}`);
+
+//task 7
+console.log(`task 7: the result of false || false is ${false || false}`);
+
+//task 8
+console.log(`task 8: the not (!) operator change any true/truthy value to false and any false/falsy value to true`);
+
+//task 9
+console.log(`task 9: the result of !true is ${!true}`);
+
+//task 10
+console.log(`task 10: the result of !false is ${!false}`);
+
+//task 11
+console.log(`task 11: the result of true && true && false is ${true && true && false}`);
+
+//task 12
+console.log(`task 12: the result of false || false || true is ${false || false || true}`);
+
+//task 13
+console.log(`task 13: the order of precedence for logical operators is - not (!), and (&&) and lastly or (||)`);
+
+//task 14
+console.log(`task 14: `);
+
+//task 15
+console.log(`task 15: `);
+
+//task 16
+console.log(`task 16: `);
+
+//task 17
+console.log(`task 17: `);
+
+//task 18
+console.log(`task 18: `);
+
+//task 19
+console.log(`task 19: `);
+
+//task 20
+console.log(`task 20: `);
+}
