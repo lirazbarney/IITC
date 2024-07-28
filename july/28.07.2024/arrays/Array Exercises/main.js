9293291d05d64b1d257af37a7e81be88250c97fc
lirazbarney <107267082+lirazbarney@users.noreply.github.com>
2024-07-28 16:29:29 +0300

    finished class

--- july/28.07.2024/arrays/Array Exercises/main.js
+++ july/28.07.2024/arrays/Array Exercises/main.js
@@ -1,115 +1,146 @@
-// //loops
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// function evenFiltering(number) {
-//     return (number % 2) === 0;
-// }
-// function addGreeting(name) {
-//     return `hello ${name}`;
-// }
-// function isPositive(number) {
-//     return number > 0;
-// }
-// function greaterThan10(number) {
-//     return number > 10;
-// }
-// function isBanana(fruit) {
-//     return fruit === `Banana`;
-// }
-// function getProduct(params) {
-    
-// }
+//loops
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function evenFiltering(number) {
+    return (number % 2) === 0;
+}
+function addGreeting(name) {
+    return `hello ${name}`;
+}
+function isPositive(number) {
+    return number > 0;
+}
+function greaterThan10(number) {
+    return number > 10;
+}
+function isBanana(fruit) {
+    return fruit === `Banana`;
+}
+function getProduct(product, number) {
+    return product * number;
+}
+function longWord(word) {
+    return word.length > 5;
+}
 
-// //task 1
-// let fruits = [`apples`, `bananas`, `coconuts`];
-// console.log(`task 1: my fav fruits are ${fruits}`);
+//task 1
+let fruits = [`apples`, `bananas`, `coconuts`];
+console.log(`task 1: my fav fruits are ${fruits}`);
 
-// //task 2
-// fruits.push(`dragon fruits`);
-// console.log(`task 2: my new fav fruits are ${fruits}`);
+//task 2
+fruits.push(`dragon fruits`);
+console.log(`task 2: my new fav fruits are ${fruits}`);
 
-// //task 3
-// let lastFruit = fruits.pop();
-// console.log(`task 3: my updated fav fruits are ${fruits}. i don't like ${lastFruit} no more`);
+//task 3
+let lastFruit = fruits.pop();
+console.log(`task 3: my updated fav fruits are ${fruits}. i don't like ${lastFruit} no more`);
 
-// //task 4
-// let numbers = [1, 2, 3, 4, 5];
-// console.log(`task 4: the third number is ${numbers[2]}`);
+//task 4
+let numbers = [1, 2, 3, 4, 5];
+console.log(`task 4: the third number is ${numbers[2]}`);
 
-// //task 5
-// fruits[1] = `kiwi`;
-// console.log(`task 5: now, my fav fruits are ${fruits}`);
+//task 5
+fruits[1] = `kiwi`;
+console.log(`task 5: now, my fav fruits are ${fruits}`);
 
-// //task 6
-// let colors = [];
-// colors.push(`red`, `green`, `blue`);
-// console.log(`task 6: the colors are ${colors}`);
+//task 6
+let colors = [];
+colors.push(`red`, `green`, `blue`);
+console.log(`task 6: the colors are ${colors}`);
 
-// //task 7
-// let color = colors.shift();
-// console.log(`task 7: the colors are now ${colors} without ${color}`);
+//task 7
+let color = colors.shift();
+console.log(`task 7: the colors are now ${colors} without ${color}`);
 
-// //task 8
-// colors.unshift(`yellow`);
-// console.log(`task 8: the new colors are ${colors}`);
+//task 8
+colors.unshift(`yellow`);
+console.log(`task 8: the new colors are ${colors}`);
 
-// //task 9
-// let sum = 0;
-// for (i = 0; i < numbers.length; i++) {
-//     sum += numbers[i];
-// }
-// console.log(`task 9: the sum of ${numbers} is ${sum}`);
+//task 9
+let sum = 0;
+for (i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log(`task 9: the sum of ${numbers} is ${sum}`);
 
-// //task 10
-// console.log(`task 10: green is at index ${colors.indexOf(`green`)}`);
+//task 10
+console.log(`task 10: green is at index ${colors.indexOf(`green`)}`);
 
-// //task 11
-// let randomNumbers = [];
-// for (let i = 0; i < 5; i++) {
-//     randomNumbers.push(getRandomInt(-1, 15))
-// }
-// let max = randomNumbers [0];
-// for (let i = 1; i < randomNumbers.length; i++) {
-//     if (randomNumbers[i] > max) {
-//         max = randomNumbers[i]
-//     }
-// }
-// console.log(`task 11: the largest number among ${randomNumbers} is ${max}`);
+//task 11
+let randomNumbers = [];
+for (let i = 0; i < 5; i++) {
+    randomNumbers.push(getRandomInt(-1, 15))
+}
+let max = randomNumbers [0];
+for (let i = 1; i < randomNumbers.length; i++) {
+    if (randomNumbers[i] > max) {
+        max = randomNumbers[i]
+    }
+}
+console.log(`task 11: the largest number among ${randomNumbers} is ${max}`);
 
-// //task 12
-// let newFruits = fruits.slice(1, 3);
-// console.log(`task 12: my best fav fruits are ${newFruits}`);
+//task 12
+let newFruits = fruits.slice(1, 3);
+console.log(`task 12: my best fav fruits are ${newFruits}`);
 
-// //task 13
-// let evenNumbers = numbers.filter(evenFiltering);
-// console.log(`task 13: the numbers are ${numbers} but the even numbers are ${evenNumbers}`);
+//task 13
+let evenNumbers = numbers.filter(evenFiltering);
+console.log(`task 13: the numbers are ${numbers} but the even numbers are ${evenNumbers}`);
 
-// //task 14
-// let colorsStr = colors.join(`, `);
-// console.log(`task 14: my colors are ${colorsStr}`);
+//task 14
+let colorsStr = colors.join(`, `);
+console.log(`task 14: my colors are ${colorsStr}`);
 
-// //task 15
-// let newNumbers = numbers.concat(randomNumbers);
-// console.log(`task 15: the numbers are ${numbers}, the random numbers are ${randomNumbers} and the concocted numbers array is ${newNumbers}`);
+//task 15
+let newNumbers = numbers.concat(randomNumbers);
+console.log(`task 15: the numbers are ${numbers}, the random numbers are ${randomNumbers} and the concocted numbers array is ${newNumbers}`);
 
-// //task 16
-// fruits.reverse();
-// console.log(`task 16: my new order of fav fruits are ${fruits}`);
+//task 16
+fruits.reverse();
+console.log(`task 16: my new order of fav fruits are ${fruits}`);
 
-// //task 17
-// let names = [`liraz`, `josef`, `daniel`];
-// let greetNames = names.map(addGreeting);
-// console.log(`task 17: the names are ${names}, the mapped names are ${greetNames}`);
+//task 17
+let names = [`liraz`, `josef`, `daniel`];
+let greetNames = names.map(addGreeting);
+console.log(`task 17: the names are ${names}, the mapped names are ${greetNames}`);
 
-// //task 18
-// console.log(`task 18: all the numbers in numbers are ${randomNumbers.every(isPositive)}`);
+//task 18
+console.log(`task 18: all the numbers in numbers are ${randomNumbers.every(isPositive)}`);
 
-// //task 19
-// console.log(`task 19: the first number grater than 10 is ${randomNumbers.find(greaterThan10)}`);
+//task 19
+console.log(`task 19: the first number grater than 10 is ${randomNumbers.find(greaterThan10)}`);
 
-// //task 20
-// console.log(`task 20: i have a banana in my fruits is ${fruits.some(isBanana)}`);
+//task 20
+console.log(`task 20: i have a banana in my fruits is ${fruits.some(isBanana)}`);
 
-// //task 21
-// console.log(`task 21: `);
+//task 21
+console.log(`task 21: the product of the array of numbers ${numbers} is ${numbers.reduce(getProduct, 1)}`);
+
+//task 22
+let sqrs = [];
+for (let i = 1; i <= 10; i++){
+    sqrs.push(i * i);
+}
+console.log(`task 22: the sqrs is ${sqrs}`);
+
+//task 23
+let words = [`hello`, `worlds`, `lirazing`, `abc`, `abcdefghijklmnopqrstuvxyz`];
+let longWords = words.filter(longWord);
+console.log(`task 23: words are ${words}, the long words are ${longWords}`);
+
+//task 24
+console.log(`task 24: ${colors} contain "purple" - ${colors.includes(`purple`)}`);
+
+//task 25
+let xo = [[`x`, `o`, ``], [``, `x`, `o`], [``, ``, `x`]];
+function printXO() {
+    console.log(`task 25`);
+    console.log(`
+${xo[0][0]} | ${xo[0][1]} | ${xo[0][2]}
+-------
+${xo[1][0]} | ${xo[1][1]} | ${xo[1][2]}
+-------
+${xo[2][0]} | ${xo[2][1]} | ${xo[2][2]}`);
+}
+printXO();
---
+++ july/28.07.2024/arrays/onclass/main.js
@@ -0,0 +1,33 @@
+//1. create an array of your five fav foods
+let foods = [`pasta`, `chicken`, `cornflakes`, `cheese`, `hamburger`];
+
+//2. access and print the third item in your fav foods array
+console.log(foods[2]);
+
+//3. change the second item in your array to a different food
+foods[1] = `cow`;
+
+//4. add a new food to the end of your array using push()
+foods.push(`chicken`);
+
+//5. remove the first food from your array using shift
+console.log(foods[0]);
+let food = foods.pop();
+console.log(foods[0]);
+
+
+//6. create a loop that prints all the foods in your array
+for (let i = 0; i < foods.length; i++) {
+    console.log(foods[i]);
+}
+
+//7. find the index of a specific food in your array using indexof()
+console.log(`cheese is at ${foods.indexOf(`cheese`)}`);
+
+//8. create an array of numbers and use a loop to calculate their sum
+let sum = 0;
+let numbers= [1, 2, 3, 4, 5];
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log(sum);
