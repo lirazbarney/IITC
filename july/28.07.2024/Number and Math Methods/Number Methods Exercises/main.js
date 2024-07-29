11361976a8e9384f54d9a2e4666b16de736a70c8
lirazbarney <107267082+lirazbarney@users.noreply.github.com>
2024-07-29 12:34:23 +0300

    finished number methods

---
+++ july/28.07.2024/Number and Math Methods/Number Methods Exercises/main.js
@@ -0,0 +1,76 @@
+//task 1
+console.log(`task 1: 3.14159.number.toFixed(2) is exactly ${3.14159.toFixed(2)}`);
+
+//task 2
+console.log(`task 2: typeof (42).toString() is ${typeof (42).toString()}`);
+
+//task 3
+console.log(`task 3: 123.456789.toPrecision(5) is exactly ${123.456789.toPrecision(5)}`);
+
+//task 4
+console.log(`task 4: typeof parseInt("10") is ${typeof parseInt("10")}`);
+
+//task 5
+console.log(`task 5: typeof parseFloat("3.14") is ${typeof parseFloat("3.14")}`);
+
+//task 6
+console.log(`task 6: isNaN("Hello") is ${isNaN("Hello")}`);
+
+//task 7
+console.log(`task 7: isFinite(100) is ${isFinite(100)}`);
+
+//task 8
+console.log(`task 8: (255).toString(16) is ${(255).toString(16)}`);
+
+//task 9
+console.log(`task 9: 0.000001.toFixed(7) is ${0.000001.toFixed(7)}`);
+
+//task 10
+console.log(`task 10: parseInt("1010", 2) is ${parseInt("1010", 2)}`);
+
+//task 11
+console.log(`task 11: 0.000123456.toPrecision(3) is ${0.000123456.toPrecision(3)}`);
+
+//task 12
+console.log(`task 12: isNaN(0 / 0) is ${isNaN(0 / 0)} but it is not an error for some reason (0/0 in js is infinity)`);
+
+//task 13
+console.log(`task 13: (1000000).toExponential(2) is ${(1000000).toExponential(2)}`);
+
+//task 14
+console.log(`task 14: parseFloat("3.14some") is ${parseFloat("3.14some")}`);
+
+//task 15
+console.log(`task 15: (16).toString(2) is ${(16).toString(2)}`);
+
+//task 16
+console.log(`task 16: isFinite(Infinity) is ${isFinite(Infinity)}`);
+
+//task 17
+console.log(`task 17: (1.23e-5).toFixed(8) is ${(1.23e-5).toFixed(8)}`);
+
+//task 18
+console.log(`task 18: parseInt("777", 8) is ${parseInt("777", 8)}`);
+
+//task 19
+console.log(`task 19: (123456.789).toPrecision(4) is ${(123456.789).toPrecision(4)}`);
+
+//task 20
+console.log(`task 20: isNaN("NaN") is ${isNaN("NaN")}`);
+
+//task 21
+console.log(`task 21: typeof (-10).toString() is ${typeof (-10).toString()}`);
+
+//task 22
+console.log(`task 22: parseFloat("1.2e-3") is ${parseFloat("1.2e-3")}`);
+
+//task 23
+console.log(`task 23: (1234.5678).toFixed(2) is ${(1234.5678).toFixed(2)}`);
+
+//task 24
+console.log(`task 24: isFinite(1 / 0) is ${isFinite(1 / 0)} and not an error`);
+
+//task 25
+console.log(`task 25: parseInt("FF", 16) is ${parseInt("FF", 16)}`);
+
+
