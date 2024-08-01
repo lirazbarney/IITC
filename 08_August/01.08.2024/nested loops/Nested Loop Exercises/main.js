37c94bc33157be051a64c8bbdceced3ee1e55aeb
lirazbarney <107267082+lirazbarney@users.noreply.github.com>
2024-08-01 13:51:59 +0300

    new world order hehehehe

---
+++ 08_August/01.08.2024/nested loops/Nested Loop Exercises/main.js
@@ -0,0 +1,11 @@
+//task 1
+for (let i = 0; i < 2; i++) {
+  let line = ``;
+  for (let j = 0; j < 2; j++) {
+    line += `* `;
+  }
+  console.log(`task 1: (${i}) ${line}`);
+}
+console.log(``);
+
+//task 2
