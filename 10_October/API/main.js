cfb96e7e62a74698d3e73ac405564171772d3f27
lirazbarney <107267082+lirazbarney@users.noreply.github.com>
2024-10-08 22:42:56 +0300

    finished today API homeworks

---
+++ 10_October/API/main.js
@@ -0,0 +1,25 @@
+function uploadCharacter(name, imgSrc) {
+  console.log(name, imgSrc);
+  const firstAPI = document.querySelector(".rick-morty");
+  firstAPI.querySelector("span").textContent = name;
+  firstAPI.querySelector("img").setAttribute("src", imgSrc);
+}
+function uploadingJoke(joke) {
+  console.log(joke);
+  const secondAPI = document.querySelector(".chuck-norris");
+  secondAPI.querySelector("span").textContent = joke;
+}
+
+fetch("https://rickandmortyapi.com/api/character/693")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    uploadCharacter(data.name, data.image);
+  });
+
+fetch("https://api.chucknorris.io/jokes/random")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    uploadingJoke(data.value);
+  });
