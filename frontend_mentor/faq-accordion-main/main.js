90529cf316678ee85c954e24218b86728747efc6
lirazbarney <107267082+lirazbarney@users.noreply.github.com>
2024-10-10 00:10:39 +0300

    done the HTML & js dom for another frontend mentor

--- frontend_mentor/faq-accordion-main/main.js
+++ frontend_mentor/faq-accordion-main/main.js
@@ -1,1 +1,26 @@
-console.log("hello");
+function toggleAnswer(questionNumber) {
+  console.log(questionNumber);
+  const answer = document.querySelector(`#a${questionNumber}`);
+  console.log(answer);
+  const isAlreadyHidden = answer.classList.contains("hidden");
+  HideAllAnswers();
+  if (isAlreadyHidden) {
+    answer.classList.remove("hidden");
+  }
+}
+
+function HideAllAnswers() {
+  for (let i = 1; i < 5; i++) {
+    const answer = document.querySelector(`#a${i}`);
+    answer.classList.add("hidden");
+    answer.addEventListener("click", function () {
+      HideAllAnswers();
+    });
+  }
+}
+
+for (let i = 1; i < 5; i++) {
+  document.querySelector(`#q${i}`).addEventListener("click", function () {
+    toggleAnswer(i);
+  });
+}
